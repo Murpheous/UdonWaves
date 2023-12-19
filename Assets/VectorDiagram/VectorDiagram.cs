19de55f6cbdd4b2260be0ffb8424235fb94f3c72
John K N Murphy <j.murpheous@gmail.com>
2023-12-20 12:00:28 +1300

    Side demo

---
+++ Assets/VectorDiagram/VectorDiagram.cs
@@ -0,0 +1,17 @@
+﻿
+using TMPro;
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class VectorDiagram : UdonSharpBehaviour
+{
+    public Slider stepControl;
+    [SerializeField] TextMeshProUGUI txtAngle;
+    void Start()
+    {
+        
+    }
+}
